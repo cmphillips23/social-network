72bab7514e31c4dc3c15dcc95115c5b12e5e5c55
Cnidarian <cmphillips23@gmail.com>
2021-05-03 01:12:58 -0500

    added user and thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(removeThought);
+
+module.exports = router;
